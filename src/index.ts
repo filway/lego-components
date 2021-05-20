ed70ba57ae4f518301fa5f6191f9fb5bc4d05b16
filway <filway@126.com>
2021-05-21 00:01:17 +0800

    fix: 优化导出

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 import { App } from 'vue'
+export { textDefaultProps , textStylePropNames, TextComponentProps,
+  imageDefaultProps, imageStylePropsNames, ImageComponentProps,
+  shapeDefaultProps, shapeStylePropsNames, ShapeComponentProps,
+  AllComponentProps
+} from './defaultProps'
+
 import LText from './components/LText'
 import LImage from './components/LImage'
 import LShape from './components/LShape'
